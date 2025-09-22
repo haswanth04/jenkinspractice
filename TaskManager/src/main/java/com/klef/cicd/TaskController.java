ae3b36373415f674527121481e52c6e77cd5f95b
haswanth <haswnth.chinni@gmail.com>
2025-09-23 00:19:50 +0530

    first commit

--- TaskManager/src/main/java/com/klef/cicd/TaskController.java
+++ TaskManager/src/main/java/com/klef/cicd/TaskController.java
@@ -1,11 +1,20 @@
 package com.klef.cicd;
 
-import org.springframework.web.bind.annotation.*;
 import java.util.List;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/tasks")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = {"http://localhost:5173", "http://localhost:2030"})
 public class TaskController {
     private final TaskService service;
 
--- TaskManager/src/main/java/com/klef/cicd/TaskManagerApplication.java
+++ TaskManager/src/main/java/com/klef/cicd/TaskManagerApplication.java
@@ -2,13 +2,20 @@ package com.klef.cicd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TaskManagerApplication {
+public class TaskManagerApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 		SpringApplication.run(TaskManagerApplication.class, args);
 		System.out.println("project is running!");
 		}
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(TaskManagerApplication.class);
+    }
 
 }
